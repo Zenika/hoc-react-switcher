9e61e926dc39b80d4ff8cfd79f795c91907121ce
FRIN Yvonnick <frin.yvonnick@gmail.com>
2016-12-09 18:22:31 +0100

    🐛  Fix issues #3, #4

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,19 @@ export default (config) => {
   const {
     prop = 'state',
     states = {},
+    DefaultComponent,
   } = config || {}
 
   return () => {
     const propValue = this.props[prop]
-    if (propValue && states[propValue]) {
-      const ComposedComponent = states[propValue]
-      return <ComposedComponent {...this.props} />
+
+    if (propValue === undefined && !DefaultComponent) {
+      throw new Error(`Your prop value should match one of the \`states\` or
+      you should provide a \`DefaultComponent\``)
     }
-    return null
+    if (propValue === undefined || !states[propValue]) return <DefaultComponent {...this.props} />
+
+    const ComposedComponent = states[propValue]
+    return <ComposedComponent {...this.props} />
   }
 }
